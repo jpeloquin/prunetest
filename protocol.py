f6da8c5505cc393b22c57533cb0affb0eabe3759
John Peloquin <john.peloquin@gmail.com>
2022-01-12 22:02:44 -0500

    Use "h" for hours and "μ" in Pint unit registry

--- protocol.py
+++ protocol.py
@@ -1,5 +1,6 @@
 # Base packages
 import operator
+from pathlib import Path
 from collections import OrderedDict, abc
 
 # Third-party packages
@@ -10,7 +11,23 @@ import pint
 from scipy.optimize import minimize
 
 
-ureg = pint.get_application_registry()
+def fix_pint_registry(ureg: pint.registry.ApplicationRegistry):
+    """Update pint ApplicationRegistry with some fixes"""
+    # Use "h" for hours, not for the Planck constant.  There's an open debate on this
+    # upstream: https://github.com/hgrecco/pint/issues/719
+    ureg.define('@alias hour = h')
+    # Add greek small letter mu as valid micro prefix.  Eventually this should get
+    # fixed upstream.  https://github.com/hgrecco/pint/pull/1347.  We have to use the
+    # _registry parameter due to https://github.com/hgrecco/pint/pull/1403.
+    on_redefinition = ureg._registry._on_redefinition
+    ureg._registry._on_redefinition = "ignore"
+    ureg.define('micro- = 1e-6  = µ- = μ- = u-')
+    ureg._registry._on_redefinition = on_redefinition
+    return ureg
+
+
+# noinspection PyTypeChecker
+ureg = fix_pint_registry(pint.get_application_registry())
 
 
 class ControlConflictError(ValueError):
